5734f7fccb3a70c9816afd30a5e82edfe537806b
Sidoine De Wispelaere <sidoine@sidoine.net>
2017-11-12 12:24:23 +0100

    Remove unused imports

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 import { AceDatabase } from "@wowts/ace_db-3.0";
-import { LuaObj, pairs } from "@wowts/lua";
-import { Constructor, Library} from "@wowts/tslib";
 
 const lib = {
     GetOptionsTable(db: AceDatabase): any {
