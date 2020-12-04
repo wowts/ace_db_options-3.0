634bec44dda0d7258384a4b2ad96dee26a97d624
Sidoine De Wispelaere <sidoine@sidoine.net>
2020-12-04 13:00:18 +0100

    Cleaning

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-import { AceDatabase } from "@wowts/ace_db-3.0";
-declare const lib: {
-    GetOptionsTable(db: AceDatabase): any;
-};
-export default lib;
--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const lib = {
-    GetOptionsTable(db) {
-        return {};
-    },
-};
-exports.default = lib;
